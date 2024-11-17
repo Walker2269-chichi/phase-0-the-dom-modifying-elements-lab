003548a9f6c31166708f5892d5db811bce3133f4
Walker2269-chichi <langatjeremiah22@gmail.com>
2024-11-17 18:45:58 +0300

    Dom-modifying

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Select and remove the main element with id 'main'
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+const newHeader = document.createElement('h1');
+// Set the id
+
+newHeader.id = 'victory';
+// Set the text content of the <h1> element
+newHeader.textContent = 'Jeremiah is the champion';
+
+
+// Append the newHeader to the document body
+document.body.appendChild(newHeader);
